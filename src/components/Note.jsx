34154a1d00a7db3ed6c1d4cea3b3c505239d33eb
k_miymaoto <canary16@gmail.com>
2020-03-23 17:18:02 +0900

    KeeperApp メモ部分追加

---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>ここにはノートのタイトルが入ります</h1>
+      <p>ここにはノートの内容が入ります</p>
+    </div>
+  );
+}
+export default Note;
