711fd38860e46c94409369622e039a077ca037ec
k_miymaoto <canary16@gmail.com>
2020-03-23 15:56:02 +0900

    KeeperApp header,css追加

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from "./components/App";
 
-const name = "食材";
-const num = 7;
+ReactDOM.render(<App />, document.getElementById("root"));
 
-ReactDOM.render(
-  <div>
-    <h1>お気に入りの{name}</h1>
-    <ul>
-      <li>オレンジ</li>
-      <li>いちご</li>
-      <li>ぶどう</li>
-    </ul>
-    <p>あなたのラッキーナンバーは{Math.floor(Math.random()*10)}</p>
-  </div>,
-  document.getElementById("root")
-);
