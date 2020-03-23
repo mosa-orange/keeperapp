75bdf2024f208c9cce4f4f9dffd9d106c1962ec7
k_miymaoto <canary16@gmail.com>
2020-03-23 11:30:09 +0900

    作業途中

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const name = "食材";
+const num = 7;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <div>
+    <h1>お気に入りの{name}</h1>
+    <ul>
+      <li>オレンジ</li>
+      <li>いちご</li>
+      <li>ぶどう</li>
+    </ul>
+    <p>あなたのラッキーナンバーは{Math.floor(Math.random()*10)}</p>
+  </div>,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
